60bd315a61fd3208227f7c34f2eab81a8b3d85ee
Óscar R. Garrucho <oscar.garrucho@gmail.com>
2020-12-02 23:00:50 +0100

    [MainTabController] Setup

---
+++ Marvel/App/MainTabController.swift
@@ -0,0 +1,58 @@
+//
+//  MainTabController.swift
+//  Marvel
+//
+//  Created by Oscar Rodriguez Garrucho on 2/12/20.
+//  Copyright © 2020 Little Monster. All rights reserved.
+//
+
+import UIKit
+
+class MainTabController: UITabBarController {
+
+    // MARK: - Properties
+
+    // MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+
+    // MARK: - Helpers
+
+    func configureUI() {
+        configureViewControllers()
+
+        view.backgroundColor = .white
+    }
+
+    func configureViewControllers() {
+
+//        let feed = FeedController(collectionViewLayout: UICollectionViewFlowLayout())
+//        let feedNav = templateNavigationController(image: UIImage(named: "home_unselected"),
+//                                                   rootViewController: feed)
+
+        let feed = ViewController()
+        let feedNav = templateNavigationController(image: UIImage(systemName: "list.bullet"),
+                                                   rootViewController: feed)
+
+        let favourites = ViewController()
+        let favouritesNav = templateNavigationController(image: UIImage(systemName: "star"),
+                                                      rootViewController: favourites)
+
+        viewControllers = [feedNav, favouritesNav]
+    }
+
+    func templateNavigationController(image: UIImage?,
+                                      rootViewController: UIViewController) -> UINavigationController {
+
+        let navController = UINavigationController(rootViewController: rootViewController)
+        navController.tabBarItem.image = image
+        navController.tabBarItem.title = "Favourites"
+        navController.navigationBar.barTintColor = .white
+
+        return navController
+    }
+
+}
